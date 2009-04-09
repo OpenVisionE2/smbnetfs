6a2c92250619553c490f5ed5e3b8f227ee37ce90
Mikhail Kshevetskiy <kshevetskiy@users.sourceforge.net>
2009-04-10 01:20:10 +0400

    Revert "copy-paste is an evil..."
    This reverts commit 44b3c0fe36ea0448ad83d712188a4a6bc6f01631.

--- src/smbitem.c
+++ src/smbitem.c
@@ -678,7 +678,7 @@ int smbitem_get_group(const char *host, char *buf, size_t size){
 
     if (host == NULL) return -1;
 
-    DPRINTF(6, "group=%s\n", host);
+    DPRINTF(6, "host=%s\n", host);
 
     while(*host == '/') host++;
     if (*host == '\0') return -1;
